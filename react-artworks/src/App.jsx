e261b3622b6b4eae98a09545c0b45e16a1756089
Balázs Tildy <balazs.tild@gmail.com>
2022-11-28 15:54:45 +0100

    fetching data

--- react-artworks/src/App.jsx
+++ react-artworks/src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [objectId, setObjectId] = useState([]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+
+  const getData = async () => {
+    const response = await fetch(
+      "https://collectionapi.metmuseum.org/public/collection/v1/search?q=GustavKlimt"
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  };
+
+  const init = async () => {
+    const data = await getData();
+    setObjectId(data.objectIDs);
+  };
+
+  const test = async () => {
+    for (let i = 0; i < objectId.length; i++) {
+      const response = await fetch(
+        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId[i]}`
+      );
+      const data = await response.json();
+      if (data.primaryImage !== "") {
+        console.log(data);
+      }
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  useEffect(() => {
+    test();
+  }, [objectId]);
+
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
